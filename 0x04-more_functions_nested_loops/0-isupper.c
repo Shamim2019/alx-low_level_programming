0a1ed31b4e9bab97b172424e77bc9ca13b4637d3
Shamim2019 <shamimmaloba@gmail.com>
2022-03-17 13:44:11 +0300

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,3 @@
-
-  
 #include "main.h"
 
 /**
@@ -7,7 +5,7 @@
   * @x: The number to be checked
   *
   * Return: 1 for upper letter or 0 for any else
-  */
+*/
 int _isupper(int x)
 {
 	if (x >= 65 && x <= 90)
